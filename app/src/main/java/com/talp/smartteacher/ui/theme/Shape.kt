70ba2c5d73c6780fc73eafb5f201399928b34e56
samerjilo8-del <samerjilo8@gmail.com>
2026-08-11 18:16:32 +0300

    Add UI Theme: Color, Shape, and Typography

---
+++ app/src/main/java/com/talp/smartteacher/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.talp.smartteacher.ui.theme
+
+import androidx.compose.material3.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val TalpShapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(12.dp)
+)
